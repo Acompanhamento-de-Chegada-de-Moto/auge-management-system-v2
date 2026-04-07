0b20db06dddb09615c756fee55db45edb8d6bff1
Adones Balica <adonesbalica@gmail.com>
2026-04-07 18:43:28 -0300

    feat: initial commit

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.url(),
   },
   client: {},
   experimental__runtimeEnv: {},
