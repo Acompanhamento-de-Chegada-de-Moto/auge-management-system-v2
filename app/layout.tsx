0a173f9d3489441bcf5920ec1169397df0a086b8
Adones Balica <adonesbalica@gmail.com>
2026-04-08 20:00:47 -0300

    feat: restructure RootLayout to include Header and NavLinks components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/layout/Header";
+import { NavLinks } from "@/components/layout/NavLinks";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="bg-[#f7f9fa] min-h-screen">
+          <Header />
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <NavLinks />
+          </div>
+          {children}
+          {/* <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            pauseOnHover
+          /> */}
+        </main>
+      </body>
     </html>
   );
 }
